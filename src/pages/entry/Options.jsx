6144f2018eb987cb9145f5ea561d59fd6d68d0cd
shzawa <ozawa.shotaro0114@gmail.com>
2022-09-07 13:19:27 +0900

    add: Optionコンポーネント

---
+++ src/pages/entry/Options.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Row } from 'react-bootstrap';
+import ScoopOption from './ScoopOption';
+
+const Options = ({ optionType }) => {
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => {
+        setItems(response.data);
+      })
+      .catch((error) => {
+        // TODO: handle error response
+      });
+  }, [optionType]);
+
+  const ItemComponent = optionType === 'scoops' ? ScoopOption : null;
+
+  const optionItems = items.map((item) => (
+    <ItemComponent key={item.name} {...item} />
+  ));
+
+  return <Row>{optionItems}</Row>;
+};
+
+export default Options;
